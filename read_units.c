7d89c666dcd80e751487b46aa0275c9bdf7d58e9
Leonid Benkevitch <lbenkev@gmail.com>
2023-04-10 14:10:16 -0400

    Trying to run Levine/purecalc - pure scanner and pure parser.

--- read_units.c
+++ read_units.c
@@ -3,7 +3,14 @@
 #include "read_units.tab.h"
 
 int main(int argc, char **argv) {
-       
+    
+    yyscan_t scanner;
+    
+    yylex_init(&scanner);
+    yyset_in(fopen(argv[1], "rb"), scanner);
+    yylex(scanner);
+    yylex_destroy(scanner);
+    
     yyparse();
 
     printf("Done.\n");
