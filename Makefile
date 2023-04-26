0f1e3154227aedec698bb56ce14e40aef5ee2105
Leonid Benkevitch <lbenkev@gmail.com>
2023-04-26 10:21:51 -0400

    Not reentrant. Compiles no errors.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-ru:	read_units.y read_units.l
+ru:	read_units.y read_units.l read_units.c
 	bison -dt read_units.y
 	flex -o read_units.lex.c read_units.l
 	gcc -g read_units.c read_units.tab.c read_units.lex.c read_units_funcs.c \
-		-o ru
+		-lm -o ru
 
 clean:
 	rm -f read_units.tab.h read_units.tab.c read_units.lex.h read_units.lex.c ru
--- read_units.c
+++ read_units.c
@@ -8,33 +8,43 @@
 
 int main(void) {
 
-    char meas_exp[] = "cm*((mg^2*us^-3/nA)^-5*K^5/cd/(mole*THz)^3*day)^2*" \
-        "rad*Jy^(7+2*(4-6))";
+    char const meas_exp[] = "cm*((mg^2*us^-3/nA)^-5*K^5/cd/" \
+        "(mole*THz)^3*day)^2*rad*Jy^(7+2*(4-6))";
     
-    /* int tok = 0; */
     
     YY_BUFFER_STATE buf;
 
     buf = yy_scan_string(meas_exp);
 
     printf("Measurement expression:\n \"%s\"\n\n", meas_exp);
-    
-    /* printf("Lexical analysis:\n"); */
-    /* while(tok = yylex()) */
-    /*     switch (tok) { */
-    /*     case T_symbol: */
-    /*         printf("%s ", yylval.s); break; */
-    /*     case T_number: */
-    /*         printf("%d ", yylval.d); break; */
-    /*     case '+': case '-': case '*': case '/': */
-    /*     case '^': case '(': case ')': */
-    /*         printf("'%c' ", tok); break; */
-    /*     } */
 
-    yyparse();
+    int tok = 0;
+
+    printf("Lexical analysis:\n");
+    
+    while(tok = yylex())
+        switch (tok) {
+        case T_symbol:
+            printf("%s ", yylval.s); break;
+        case T_number:
+            printf("%d ", yylval.d); break;
+        case '+': case '-': case '*': case '/':
+        case '^': case '(': case ')':
+            printf("'%c' ", tok); break;
+        }
+
+    yy_delete_buffer(buf);
+
+    /*
+     * Parse
+     */
+    buf = yy_scan_string(meas_exp);
+
+    //yyparse();
     
     printf("\n");
     
     yy_delete_buffer(buf);
+    
     return 0;
 }
